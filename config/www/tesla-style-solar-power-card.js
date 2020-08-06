ed90d524809edc78133ab173f967ef1ad099e653
reptilex <reptilex@gmail.com>
2020-08-06 15:19:39 +0200

    no logger anymore

--- config/www/tesla-style-solar-power-card.js
+++ config/www/tesla-style-solar-power-card.js
@@ -408,7 +408,7 @@ br.clear {
   }
 
   updateOneCircle(timestamp, entity) {
-    console.log('updatingOneCircle speed:' + entity.speed + " maxpos:" + entity.maxPosition);
+    //console.log('updatingOneCircle speed:' + entity.speed + " maxpos:" + entity.maxPosition);
     //console.log('updatingOneCircle beg pos:' + entity.currentPosition);
     if (this.clientWidth !== 0) {
       //entity.maxPosition = 2 * this.clientWidth - 570;
