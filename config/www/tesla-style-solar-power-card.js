d86c8a99e1cb6517080ca53f1a64e6d9cc820b05
reptilex <reptilex@gmail.com>
2020-09-02 20:55:47 +0200

    hook check

--- config/www/tesla-style-solar-power-card.js
+++ config/www/tesla-style-solar-power-card.js
@@ -136,7 +136,7 @@ class TeslaStyleSolarPowerCard extends HTMLElement {
   }
 
   /*connectedCallback() {
-    //checking hook 3
+    //checking hook 7
     if(!this.contentIsCreated) return;
 
     console.log("testing callback");
