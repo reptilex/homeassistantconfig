b178e251c93abac884843f5440be69d7ea657b21
reptilex <reptilex@gmail.com>
2020-09-02 20:56:01 +0200

    Committing automatic changes done through UI

--- config/www/tesla-style-solar-power-card.js
+++ config/www/tesla-style-solar-power-card.js
@@ -136,7 +136,7 @@ class TeslaStyleSolarPowerCard extends HTMLElement {
   }
 
   /*connectedCallback() {
-    //checking hook 7
+    //checking hook 3
     if(!this.contentIsCreated) return;
 
     console.log("testing callback");
